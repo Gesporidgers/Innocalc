ac9477b8ec92d6c5ac85777924c6fff406fd5569
Gesporidgers <49988107+Gesporidgers@users.noreply.github.com>
2025-07-18 10:57:48 +0300

    Удалён код линейной интерполяции

--- Utility/FileSearcher.cs
+++ Utility/FileSearcher.cs
@@ -12,25 +12,6 @@ namespace Innocalc.Utility
 			CubicSpline cubicSpline = CubicSpline.InterpolateAkima(Dict.Keys.AsEnumerable(), Dict.Values.AsEnumerable());
 			float res = (float)cubicSpline.Interpolate(val);
 			return res;
-			var keys = Dict.Keys.ToList();
-			keys.Sort();
-			/*if (val < keys[0])
-				return Dict[keys[0]];
-			else if (val > keys[^1])
-				return Dict[keys[^1]];
-			else if (Dict.ContainsKey(val))
-				return Dict[val];
-			else
-			{
-				int lowerKey = keys.Where(k => k < val).Max();
-				int upperKey = keys.Where(k => k > val).Min();
-
-				float lowerValue = Dict[lowerKey];
-				float upperValue = Dict[upperKey];
-
-				float interpolatedValue = lowerValue + (upperValue - lowerValue) * (val - lowerKey) / (upperKey - lowerKey);
-				return interpolatedValue;
-			}*/
 		}
 	}
 }
