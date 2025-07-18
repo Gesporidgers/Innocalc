1e6052444591e7b3ecd4efe44bbd21fd1bb7ac9f
Gesporidgers <49988107+Gesporidgers@users.noreply.github.com>
2025-07-18 08:41:08 +0300

    Создана кубическая интерполяция искомых значений

--- Utility/FileSearcher.cs
+++ Utility/FileSearcher.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Text.Json;
+using MathNet.Numerics.Interpolation;
 
 namespace Innocalc.Utility
 {
@@ -7,10 +8,13 @@ namespace Innocalc.Utility
 	{
 		public static float Search(int val, string file)
 		{
-			var Dict = JsonSerializer.Deserialize<Dictionary<int, float>>(File.ReadAllText("Data\\" + file));
+			var Dict = JsonSerializer.Deserialize<Dictionary<double, double>>(File.ReadAllText("Data\\" + file));
+			CubicSpline cubicSpline = CubicSpline.InterpolateAkima(Dict.Keys.AsEnumerable(), Dict.Values.AsEnumerable());
+			float res = (float)cubicSpline.Interpolate(val);
+			return res;
 			var keys = Dict.Keys.ToList();
 			keys.Sort();
-			if (val < keys[0])
+			/*if (val < keys[0])
 				return Dict[keys[0]];
 			else if (val > keys[^1])
 				return Dict[keys[^1]];
@@ -26,7 +30,7 @@ namespace Innocalc.Utility
 
 				float interpolatedValue = lowerValue + (upperValue - lowerValue) * (val - lowerKey) / (upperKey - lowerKey);
 				return interpolatedValue;
-			}
+			}*/
 		}
 	}
 }
